a66d20fee9b68d64997640f00ecfd67e12d324e9
Farid Cortes <difcortesgu@unal.edu.co>
2019-03-06 15:43:46 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Estamos en la ruta por default');
+  res.send('hola');
 });
 
 module.exports = router;
