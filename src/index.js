accdb73ade631dd4f2be8a52e372ed144d226950
crabaqueropi <crabaqueropi@unal.edu.co>
2019-03-08 15:11:44 -0500

    Corregí tildes

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   layoutsDir: path.join(app.get('views'), 'layouts'),
-  //partialDir: path.join(app.get('views'), 'partials'),
+  partialDir: path.join(app.get('views'), 'partials'),
   extname: '.hbs',
   helpers: require('./lib/handlebars'),
 }));
