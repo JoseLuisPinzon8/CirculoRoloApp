50f69cf03247a4706c2e236d9e04b69a8282e1fc
crabaqueropi <crabaqueropi@unal.edu.co>
2019-03-08 16:04:17 -0500

    Base de Datos Actualizada

--- database/bd.sql
+++ database/bd.sql
@@ -9,24 +9,22 @@ DROP TABLE IF EXISTS COMENTARIO;
 DROP TABLE IF EXISTS REACCION;
 DROP TABLE IF EXISTS PARTICIPACION;
 DROP TABLE IF EXISTS EVENTO;
-DROP TABLE IF EXISTS MODERADOR;
-DROP TABLE IF EXISTS CATEGORIAS;
-DROP TABLE IF EXISTS ADMINISTRADOR;
+DROP TABLE IF EXISTS CATEGORIA;
 DROP TABLE IF EXISTS LUGAR;
 DROP TABLE IF EXISTS USUARIOS;
 
 
 CREATE TABLE IF NOT EXISTS USUARIOS (
-  id INT NOT NULL AUTO_INCREMENT,
-  userName VARCHAR(45) NOT NULL,
-  nombres VARCHAR(45) NOT NULL,
-  apellidos VARCHAR(45) NOT NULL,
-  contraseña VARCHAR(45) NOT NULL,
-  correo VARCHAR(45) NOT NULL,
-  fechaDeNacimiento datetime NOT NULL,
+  usua_id INT NOT NULL AUTO_INCREMENT,
+  usua_userName VARCHAR(45) NOT NULL,
+  usua_nombres VARCHAR(45) NOT NULL,
+  usua_apellidos VARCHAR(45) NOT NULL,
+  usua_contraseña VARCHAR(45) NOT NULL,
+  usua_correo VARCHAR(45) NOT NULL,
+  usua_fechaDeNacimiento datetime NOT NULL,
   /*edad int -- cuando se consulte (columna calculada)*/
-  foto BLOB NULL,
-  PRIMARY KEY (id))
+  usua_foto BLOB NULL,
+  PRIMARY KEY (usua_id))
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
@@ -34,55 +32,24 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS LUGAR (
-  id INT NOT NULL AUTO_INCREMENT,
-  nombre VARCHAR(45) NOT NULL,
-  direccion VARCHAR(45) NOT NULL,
-  paginaWeb VARCHAR(45) NULL,
-  latitud DOUBLE NULL,
-  longitud DOUBLE NULL,
-  PRIMARY KEY (id))
+  lug_id INT NOT NULL AUTO_INCREMENT,
+  lug_nombre VARCHAR(45) NOT NULL,
+  PRIMARY KEY (lug_id),
+  UNIQUE INDEX nombre_UNIQUE (lug_nombre ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table CATEGORIAS
+-- Table CATEGORIA
 -- -----------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS CATEGORIAS (
-  id INT NOT NULL AUTO_INCREMENT,
-  nombre VARCHAR(45) NOT NULL,
-  descripcion VARCHAR(200) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE INDEX nombre_UNIQUE (nombre ASC) VISIBLE)
+CREATE TABLE IF NOT EXISTS CATEGORIA (
+  cat_id INT NOT NULL AUTO_INCREMENT,
+  cat_nombre VARCHAR(45) NOT NULL,
+  cat_descripcion VARCHAR(200) NOT NULL,
+  PRIMARY KEY (cat_id),
+  UNIQUE INDEX nombre_UNIQUE (cat_nombre ASC) VISIBLE)
 ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table MODERADOR
--- -----------------------------------------------------
-
-
-CREATE TABLE IF NOT EXISTS MODERADOR (
-  id INT NOT NULL AUTO_INCREMENT,
-  idUsuario INT NOT NULL,
-  idCategoria INT NOT NULL,
-  /*fechaDeAscenso datetime GENERATED ALWAYS AS (sysdate()), --AL MOMENTO DE LA INSERCIÓN PONEMOS NOW()*/
-  fechaDeAscenso datetime,
-  PRIMARY KEY (id),
-  INDEX fk_usuario_idx (idUsuario ASC) VISIBLE,
-  INDEX fk_categoria_idx (idCategoria ASC) VISIBLE,
-  CONSTRAINT fk_usuario_moderador
-    FOREIGN KEY (idUsuario)
-    REFERENCES USUARIOS (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_categoria_moderador
-    FOREIGN KEY (idCategoria)
-    REFERENCES CATEGORIAS (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table EVENTO
@@ -90,42 +57,34 @@ ENGINE = InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS EVENTO (
-  id INT NOT NULL AUTO_INCREMENT,
-  nombre VARCHAR(45) NOT NULL,
-  descripcion VARCHAR(200) NOT NULL,
-  capacidad VARCHAR(45) NULL,
-  fecha datetime NOT NULL,
-  duracion INT NULL,
-  costo INT NULL,
-  foto BLOB NULL,
-  validado TINYINT NULL,
-  idUsuario INT NOT NULL,
-  idLugar INT NOT NULL,
-  idCategoria INT NOT NULL,
-  idModerador INT NOT NULL,
-  PRIMARY KEY (id),
-  INDEX fk_usuario_idx (idUsuario ASC) VISIBLE,
-  INDEX fk_lugar_idx (idLugar ASC) VISIBLE,
-  INDEX fk_categoria_idx (idCategoria ASC) VISIBLE,
-  INDEX fk_moderador_idx (idModerador ASC) VISIBLE,
+  even_id INT NOT NULL AUTO_INCREMENT,
+  even_nombre VARCHAR(45) NOT NULL,
+  even_descripcion VARCHAR(200) NOT NULL,
+  even_capacidad VARCHAR(45) NULL,
+  even_fecha datetime NOT NULL,
+  even_duracion INT NULL,
+  even_costo INT NULL,
+  even_foto BLOB NULL,
+  even_idUsuario INT NOT NULL,
+  even_idLugar INT NOT NULL,
+  even_idCategoria INT NOT NULL,
+  PRIMARY KEY (even_id),
+  INDEX fk_usuario_idx (even_idUsuario ASC) VISIBLE,
+  INDEX fk_lugar_idx (even_idLugar ASC) VISIBLE,
+  INDEX fk_categoria_idx (even_idCategoria ASC) VISIBLE,
   CONSTRAINT fk_usuario_evento
-    FOREIGN KEY (idUsuario)
-    REFERENCES USUARIOS (id)
+    FOREIGN KEY (even_idUsuario)
+    REFERENCES USUARIOS (usua_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT fk_lugar_evento
-    FOREIGN KEY (idLugar)
-    REFERENCES LUGAR (id)
+    FOREIGN KEY (even_idLugar)
+    REFERENCES LUGAR (lug_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT fk_categoria_evento
-    FOREIGN KEY (idCategoria)
-    REFERENCES CATEGORIAS (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_moderador_evento
-    FOREIGN KEY (idModerador)
-    REFERENCES MODERADOR (id)
+    FOREIGN KEY (even_idCategoria)
+    REFERENCES CATEGORIA (cat_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -143,12 +102,12 @@ CREATE TABLE IF NOT EXISTS PARTICIPACION (
   INDEX fk_evento_idx (idEvento ASC) VISIBLE,
   CONSTRAINT fk_usuario_participacion
     FOREIGN KEY (idUsuario)
-    REFERENCES USUARIOS (id)
+    REFERENCES USUARIOS (usua_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT fk_evento_participacion
     FOREIGN KEY (idEvento)
-    REFERENCES EVENTO (id)
+    REFERENCES EVENTO (even_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -167,12 +126,12 @@ CREATE TABLE IF NOT EXISTS REACCION (
   INDEX fk_evento_idx (idEvento ASC) VISIBLE,
   CONSTRAINT fk_usuario_reaccion
     FOREIGN KEY (idUsuario)
-    REFERENCES USUARIOS (id)
+    REFERENCES USUARIOS (usua_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT fk_evento_reaccion
     FOREIGN KEY (idEvento)
-    REFERENCES EVENTO (id)
+    REFERENCES EVENTO (even_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -187,18 +146,18 @@ CREATE TABLE IF NOT EXISTS COMENTARIO (
   idUsuario INT NOT NULL,
   idEvento INT NOT NULL,
   comentario VARCHAR(200) NOT NULL,
-  /*fecha datetime GENERATED ALWAYS AS (sysdate()) stored, --AL MOMENTO DE LA INSERCIÓN PONEMOS NOW()*/
+  /*fecha datetime GENERATED ALWAYS AS (sysdate()) stored, --AL MOMENTO DE LA INSERCIÓN PONEMOS NOW()*/ 
   fecha datetime,
   PRIMARY KEY (idUsuario, idEvento),
   INDEX fk_evento_idx (idEvento ASC) VISIBLE,
   CONSTRAINT fk_usuario_comentario
     FOREIGN KEY (idUsuario)
-    REFERENCES USUARIOS (id)
+    REFERENCES USUARIOS (usua_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT fk_evento_comentario
     FOREIGN KEY (idEvento)
-    REFERENCES EVENTO (id)
+    REFERENCES EVENTO (even_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -217,30 +176,13 @@ CREATE TABLE IF NOT EXISTS CATEGORIA_DE_INTERES (
   INDEX fk_categoria_idx (idCategoria ASC) VISIBLE,
   CONSTRAINT fk_categoria_intereses
     FOREIGN KEY (idCategoria)
-    REFERENCES CATEGORIAS (id)
+    REFERENCES CATEGORIA (cat_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT fk_usuario_intereses
     FOREIGN KEY (idUsuario)
-    REFERENCES USUARIOS (id)
+    REFERENCES USUARIOS (usua_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
--- -----------------------------------------------------
--- Table ADMINISTRADOR
--- -----------------------------------------------------
-
-
-CREATE TABLE IF NOT EXISTS ADMINISTRADOR (
-  id INT NOT NULL AUTO_INCREMENT,
-  idUsuario INT NOT NULL,
-  PRIMARY KEY (id),
-  INDEX fk_usuarios_idx (idUsuario ASC) VISIBLE,
-  CONSTRAINT fk_usuarios_administrador
-    FOREIGN KEY (idUsuario)
-    REFERENCES USUARIOS (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
