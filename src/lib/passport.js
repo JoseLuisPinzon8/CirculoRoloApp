7490467ce8895513d32f7bba741dd51e8cc93939
Farid Cortes <difcortesgu@unal.edu.co>
2019-03-10 18:06:20 -0500

    ingresando usuarios a la base de datos

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -7,16 +7,16 @@ passport.use('local.registro', new LocalStrategy({
   usernameField: 'usua_userName',
   passwordField: 'usua_contraseña',
   passReqToCallback: true
-}, (username, password, req, done) => {
-  console.log(req.body);
-  const { userName, nombres, apellidos, correo, fechaDeNacimiento } = req.body;
+}, (req, username, password, done) => {
+  const { userName, usua_nombres, usua_apellidos, usua_correo, usua_fechaDeNacimiento } = req.body;
+  console.log(username, usua_nombres, usua_apellidos, usua_correo, usua_fechaDeNacimiento);
   var q = 'INSERT INTO USUARIOS (usua_userName, usua_nombres, usua_apellidos, usua_contraseña, usua_correo, usua_fechaDeNacimiento) VALUES (';
   q += '"' + username + '"';
-  q += ',"' + nombres + '"';
-  q += ',"' + apellidos + '"';
+  q += ',"' + usua_nombres + '"';
+  q += ',"' + usua_apellidos + '"';
   q += ',"' + password + '"';
-  q += ',"' + correo + '"';
-  q += ',"' + fechaDeNacimiento + '");';
+  q += ',"' + usua_correo + '"';
+  q += ',"' + usua_fechaDeNacimiento + '");';
   console.log(q);
   c = 'select * from USUARIOS';
   pool.query(q, (error, results, fields)=> {
